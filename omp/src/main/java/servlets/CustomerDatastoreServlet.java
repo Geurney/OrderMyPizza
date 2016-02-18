f1cb0ff0db3863e6247ef21e142fece519b55d51
Geurney <dongqiao.ma@gmail.com>
2016-02-18 14:07:44 -0800

    Updated customer datastore servlet methods

--- omp/src/main/java/servlets/CustomerDatastoreServlet.java
+++ omp/src/main/java/servlets/CustomerDatastoreServlet.java
@@ -3,10 +3,6 @@
  */
 package servlets;
 
-/**
- * @author user
- *
- */
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -27,82 +23,43 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
 /**
- * Handle customer datastore requests
+ * Handle customer datastore requests, matching url: "/customerprofile"
  * 
  * @author Geurney
  *
  */
 public class CustomerDatastoreServlet extends HttpServlet {
 
-	/*
-	 * Handle get request
+	private static final long serialVersionUID = 5327462473337052693L;
+
+	/**
+	 * Handle get request by redirecting to "/customerprofile.jsp"
 	 * 
-	 * @see
-	 * javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest
-	 * , javax.servlet.http.HttpServletResponse)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest
+	 *      , javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		UserService userService = UserServiceFactory.getUserService();
-		String thisURL = req.getRequestURI();
-
-		resp.setContentType("text/html");
-		if (req.getUserPrincipal() != null) {
-			User user = userService.getCurrentUser();
-			resp.getWriter().println(
-					"<p>Hello, " + req.getUserPrincipal().getName()
-							+ "!  You can <a href=\""
-							+ userService.createLogoutURL(thisURL)
-							+ "\">sign out</a>.</p>");
-
-			String hash_uid = UserIDObscure.obsecure(user.getUserId());
-			DatastoreService datastore = DatastoreServiceFactory
-					.getDatastoreService();
-			Key key = KeyFactory.createKey("Customer", hash_uid);
-
-			try {
-				Entity customer = datastore.get(key);
-				String name = (String) customer.getProperty("name");
-				String address = (String) customer.getProperty("address");
-				String phone = (String) customer.getProperty("phone");
-				resp.getWriter()
-						.println(
-								"<h2>" + name + ": " + address + " " + phone
-										+ " </h2>");
-			} catch (EntityNotFoundException e) {
-				resp.getWriter().println("<h2>Error: Customer not found!</h2>");
-			}
-		} else {
-			resp.getWriter().println(
-					"<p>Please <a href=\""
-							+ userService.createLoginURL(thisURL)
-							+ "\">sign in</a>.</p>");
-		}
+		resp.sendRedirect("/customerprofile.jsp");
 	}
 
-	/*
-	 * Handle post request
+	/**
+	 * Handle post request by storing customer into datastore, redirect to
+	 * "/customerprofile.jsp". Accept param: name, address,phone. If user does
+	 * not logs in, send 403.
 	 * 
-	 * @see
-	 * javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest
-	 * , javax.servlet.http.HttpServletResponse)
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest
+	 *      , javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		UserService userService = UserServiceFactory.getUserService();
-		String thisURL = req.getRequestURI();
-
-		resp.setContentType("text/html");
 		if (req.getUserPrincipal() != null) {
 			User user = userService.getCurrentUser();
-			resp.getWriter().println(
-					"<p>Hello, " + req.getUserPrincipal().getName()
-							+ "!  You can <a href=\""
-							+ userService.createLogoutURL(thisURL)
-							+ "\">sign out</a>.</p>");
-
+			System.out.println("Get Post Request to CustomerProfile: "
+					+ req.getParameter("name"));
 			String hash_uid = UserIDObscure.obsecure(user.getUserId());
 			DatastoreService datastore = DatastoreServiceFactory
 					.getDatastoreService();
@@ -111,12 +68,8 @@ public class CustomerDatastoreServlet extends HttpServlet {
 			Entity customer = null;
 			try {
 				customer = datastore.get(key);
-				resp.getWriter().println(
-						"<h2>" + "Customer updating..." + "</h2>");
 			} catch (EntityNotFoundException e) {
 				customer = new Entity("Customer", hash_uid);
-				resp.getWriter().println(
-						"<h2>" + "Customer storing..." + "</h2>");
 			} finally {
 				String name = req.getParameter("name");
 				if (name != null) {
@@ -131,25 +84,19 @@ public class CustomerDatastoreServlet extends HttpServlet {
 					customer.setProperty("phone", phone);
 				}
 				datastore.put(customer);
-				resp.getWriter()
-						.println(
-								"<h2>" + name + ": " + address + " " + phone
-										+ " </h2>");
+				resp.sendRedirect("/customerprofile.jsp");
 			}
 		} else {
-			resp.getWriter().println(
-					"<p>Please <a href=\""
-							+ userService.createLoginURL(thisURL)
-							+ "\">sign in</a>.</p>");
+			resp.sendError(HttpServletResponse.SC_FORBIDDEN,
+					"Please log in before proceeding!");
 		}
 	}
 
-	/*
-	 * Handle put request
+	/**
+	 * Handle put request as post request.
 	 * 
-	 * @see
-	 * javax.servlet.http.HttpServlet#doPut(javax.servlet.http.HttpServletRequest
-	 * , javax.servlet.http.HttpServletResponse)
+	 * @see javax.servlet.http.HttpServlet#doPut(javax.servlet.http.HttpServletRequest
+	 *      , javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
 	protected void doPut(HttpServletRequest req, HttpServletResponse resp)
@@ -157,45 +104,28 @@ public class CustomerDatastoreServlet extends HttpServlet {
 		this.doPost(req, resp);
 	}
 
-	/*
-	 * Handle delete request
+	/**
+	 * Handle delete request by deleting the customer in datastore, sending 204.
+	 * If user does not logs in, send 403.
 	 * 
-	 * @see
-	 * javax.servlet.http.HttpServlet#doDelete(javax.servlet.http.HttpServletRequest
-	 * , javax.servlet.http.HttpServletResponse)
+	 * @see javax.servlet.http.HttpServlet#doDelete(javax.servlet.http.HttpServletRequest
+	 *      , javax.servlet.http.HttpServletResponse)
 	 */
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		UserService userService = UserServiceFactory.getUserService();
-		String thisURL = req.getRequestURI();
-
-		resp.setContentType("text/html");
 		if (req.getUserPrincipal() != null) {
 			User user = userService.getCurrentUser();
-			resp.getWriter().println(
-					"<p>Hello, " + req.getUserPrincipal().getName()
-							+ "!  You can <a href=\""
-							+ userService.createLogoutURL(thisURL)
-							+ "\">sign out</a>.</p>");
-
 			String hash_uid = UserIDObscure.obsecure(user.getUserId());
 			DatastoreService datastore = DatastoreServiceFactory
 					.getDatastoreService();
 			Key key = KeyFactory.createKey("Customer", hash_uid);
-			try {
-				Entity customer = datastore.get(key);
-				datastore.delete(key);
-				resp.getWriter().println(
-						"<h2>" + "Customer is deleted!" + "</h2>");
-			} catch (EntityNotFoundException e) {
-				resp.getWriter().println("<h2>Error: Customer not found!</h2>");
-			}
+			datastore.delete(key);
+			resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
 		} else {
-			resp.getWriter().println(
-					"<p>Please <a href=\""
-							+ userService.createLoginURL(thisURL)
-							+ "\">sign in</a>.</p>");
+			resp.sendError(HttpServletResponse.SC_FORBIDDEN,
+					"Please log in before proceeding!");
 		}
 	}
 }
